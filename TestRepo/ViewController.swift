91094b5435d114a75afae6de9a12eeb75677c3bd
Hyeonhee <hyunheeshim0103@gmail.com>
2023-10-26 00:24:36 +0900

    add print function

--- TestRepo/ViewController.swift
+++ TestRepo/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hi,there")
 
         // Do any additional setup after loading the view.
     }
