f0baf9f44d10fd588ce44fc77415f3623afef3f7
Hyeonhee <hyunheeshim0103@gmail.com>
2023-10-26 00:18:13 +0900

    Initial Commit

---
+++ TestRepo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestRepo
+//
+//  Created by 심현희 on 2023/10/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
